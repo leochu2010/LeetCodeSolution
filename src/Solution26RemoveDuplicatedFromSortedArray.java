574b245e4c70c2edde6b3cd9deb7842fbb016b9b
Leo Chu <leochu2010@gmail.com>
2024-09-07 21:34:15 +0800

    resolve problem 26

--- src/Solution26RemoveDuplicatedFromSortedArray.java
+++ src/Solution26RemoveDuplicatedFromSortedArray.java
@@ -1,22 +1,36 @@
+import java.util.Arrays;
+
 public class Solution26RemoveDuplicatedFromSortedArray {
     public static void main(String[] args) {
         new Solution26RemoveDuplicatedFromSortedArray().removeDuplicates(new int[]{1, 1, 2});
+        new Solution26RemoveDuplicatedFromSortedArray().removeDuplicates(new int[]{0,0,1,1,1,2,2,3,3,4});
     }
 
     public int removeDuplicates(int[] nums) {
+        //0,0,1,1,1,2,2,3,3,4
+        //0,1,1,1,1,2,2,3,3,4
+        //0,1,2,2,2,2,2,3,3,4
+        //0,1,2,3,3,3,3,3,3,4
+        //0,1,2,3,4,4,4,4,4,4
 
-        //loop from left to right
-        //count how many the same
-        //move all the remaining forward
-        int pointer = 0;
-        int end = nums.length;
-        while (pointer < end) {
-            int compare = pointer+1;
-            while (nums[pointer] == nums[compare]){
-                
+
+        int[] uniqueNums = new int[nums.length];
+        int lastValue = nums[0];
+        int uniqueCount = 1;
+        uniqueNums[0] = lastValue;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > lastValue) {
+                lastValue = nums[i];
+                uniqueNums[uniqueCount] = lastValue;
+                uniqueCount += 1;
             }
-
         }
-        return 1;
+        for (int i = 1; i < uniqueCount; i++) {
+            nums[i] = uniqueNums[i];
+        }
+
+        System.out.println(uniqueCount);
+        System.out.println(Arrays.toString(nums));
+        return uniqueCount;
     }
 }
